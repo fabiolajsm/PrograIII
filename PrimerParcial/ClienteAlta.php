c90d53095e858f9c31861dfd4b8aee3b79ff4af7
fabiolajsm <suarezfabiola17@gmail.com>
2023-10-22 13:31:43 -0300

    Correccion del punto 1

--- PrimerParcial/ClienteAlta.php
+++ PrimerParcial/ClienteAlta.php
@@ -11,25 +11,24 @@ de Cliente (ej.: NNNNNNTT) como identificación en la carpeta:
 */
 require './clases/Cliente.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $clienteAlta = new Cliente();
-    if (empty($_POST['nombre']) || empty($_POST['apellido']) || empty($_POST['tipoDocumento']) || empty($_POST['nroDocumento']) || empty($_POST['email']) || empty($_POST['tipoCliente']) || empty($_POST['pais']) || empty($_POST['ciudad']) || empty($_POST['telefono'])) {
-        echo "Error: tiene que ingresar todos los campos requeridos.";
-        return;
-    }
-    if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-        echo "Error: La dirección de correo electrónico no es válida.";
-        return;
-    }
-    if ($_POST['tipoCliente'] !== "individual" && $_POST['tipoCliente'] !== "corporativo") {
-        echo "Error: El Tipo de Cliente debe ser 'individual' o 'corporativo'.";
-        return;
-    }
-    if (empty($_FILES['imagen']['tmp_name'])) {
-        echo "Error: Debe proporcionar una imagen válida.";
-        return;
-    }
-    $resultado = $clienteAlta->alta($_POST);
-    echo $resultado;
+$clienteAlta = new Cliente();
+
+if (empty($_POST['nombre']) || empty($_POST['apellido']) || empty($_POST['tipoDocumento']) || empty($_POST['nroDocumento']) || empty($_POST['email']) || empty($_POST['tipoCliente']) || empty($_POST['pais']) || empty($_POST['ciudad']) || empty($_POST['telefono'])) {
+    echo "Error: tiene que ingresar todos los campos requeridos.";
+    return;
 }
+if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    echo "Error: La dirección de correo electrónico no es válida.";
+    return;
+}
+if ($_POST['tipoCliente'] !== "individual" && $_POST['tipoCliente'] !== "corporativo") {
+    echo "Error: El Tipo de Cliente debe ser 'individual' o 'corporativo'.";
+    return;
+}
+if (empty($_FILES['imagen']['tmp_name'])) {
+    echo "Error: Debe proporcionar una imagen válida.";
+    return;
+}
+$resultado = $clienteAlta->alta($_POST);
+echo $resultado;
 ?>
--- PrimerParcial/clases/Cliente.php
+++ PrimerParcial/clases/Cliente.php
@@ -1,17 +1,20 @@
 <?php
 require 'ManejadorArchivos.php';
 
-class Cliente {
+class Cliente
+{
     private $clientes;
     private $manejadorArchivos;
     private $archivoHoteles = './datos/hoteles.json';
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->manejadorArchivos = new ManejadorArchivos($this->archivoHoteles);
         $this->clientes = $this->manejadorArchivos->leer();
     }
 
-    public function alta($datos) {
+    public function alta($datos)
+    {
         $nombre = $datos["nombre"];
         $apellido = $datos["apellido"];
         $tipoDocumento = $datos["tipoDocumento"];
@@ -23,19 +26,20 @@ class Cliente {
         $telefono = $datos["telefono"];
 
         $noExisteCliente = true;
+        $clienteID = 1;
         if (!empty($this->clientes)) {
             foreach ($this->clientes as $cliente) {
-                if ($cliente["nombre"] == $nombre && $cliente["tipoCliente"] == $tipoCliente) {
+                if ($cliente["nombre"] == $nombre && $cliente["apellido"] == $apellido && $cliente["tipoCliente"] == $tipoCliente) {
                     $noExisteCliente = false;
                     return 'Error: Ya existe el cliente que quiere dar de alta.';
                 }
-            } 
+            }
+            $clienteID = intval(end($this->clientes)['id']) + 1;
         }
-        $clienteID = mt_rand(100000, 999999);
-
+        $idFormateado = str_pad($clienteID, 6, '0', STR_PAD_LEFT); // formatear para que tenga 6 digitos
         if ($noExisteCliente) {
             $nuevoCliente = [
-                "id" => $clienteID,
+                "id" => $idFormateado,
                 "nombre" => $nombre,
                 "apellido" => $apellido,
                 "tipoDocumento" => $tipoDocumento,
@@ -49,17 +53,38 @@ class Cliente {
             $this->clientes[] = $nuevoCliente;
         }
 
-        if($this->manejadorArchivos->guardar($this->clientes)){
-            $imagenID = strval($clienteID) . $tipoCliente;
+        if ($this->manejadorArchivos->guardar($this->clientes)) {
+            $imagenID = $idFormateado . $tipoCliente;
             $carpetaImagenes = './datos/ImagenesDeClientes/2023/';
             $rutaImagen = $carpetaImagenes . strtoupper($imagenID) . '.jpg';
-            if($this->manejadorArchivos->subirImagen($rutaImagen)){
+            if ($this->manejadorArchivos->subirImagen($rutaImagen)) {
                 return "Cliente registrado/actualizado exitosamente.";
             } else {
                 return "Cliente registrado/actualizado exitosamente, pero hubo un problema al guardar la imagen.";
             }
         } else {
             return "Error: No se pudo registrar u actualizar el cliente.";
+        }
+    }
+    public function consultarCliente($tipoCliente, $nroCliente)
+    {
+        if (!empty($this->clientes)) {
+            $encontrado = false;
+            foreach ($this->clientes as $cliente) {
+                if ($cliente["tipoCliente"] == $tipoCliente && $cliente["nroDocumento"] == $nroCliente) {
+                    $encontrado = true;
+                    return [
+                        "pais" => $cliente["pais"],
+                        "ciudad" => $cliente["ciudad"],
+                        "telefono" => $cliente["telefono"]
+                    ];
+                }
+            }
+            if (!$encontrado) {
+                return "No existe la combinación de tipo y número de cliente.";
+            }
+        } else {
+            return "No existen clientes registrados.";
         }
     }
 }
